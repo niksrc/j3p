4589487fcaf365c7a9d19a5b491783db0ae50945
Nikhil Srivastava <niksrc@gmail.com>
2015-11-08 16:34:11 +0530

    Adds service for ethnic collections

---
+++ public/app/ethnic.services.js
@@ -0,0 +1,71 @@
+;(function() {
+
+  angular
+    .module('core')
+    .service('Ethnic',ethnic);
+
+  ethnic.$inject = ['$http', 'Upload'];
+  
+  function ethnic(http, Upload) {
+    
+    var apiUrl = '/api/ethnic/items/';
+    var service = {
+      query: query,
+      get: get,
+      edit: edit,
+      save: save,
+      remove: remove,
+      reorder: reorder
+    }
+
+    return service;
+    
+    function query(params){
+      return http
+        .get(apiUrl,{
+          params:params
+        });
+    }
+
+    function get(id){
+      return http
+        .get(apiUrl+id);  
+    }
+
+    function edit(id, params){
+      if(params.file === undefined)
+        return http
+          .post(apiUrl+id,{
+            data: params
+          });
+      else
+        return Upload
+          .upload({
+            url: apiUrl+id,
+            data: params
+          })
+    }
+
+    function save(params){
+      return Upload
+        .upload({
+          url: apiUrl,
+          data: params
+        })
+    }
+
+    function remove(){
+      return http
+        .delete(apiUrl+id);
+    }
+
+    function reorder(params){
+      return http
+        .post(apiUrl+'reorder',{
+          data: params
+        })
+    }
+
+  }  
+
+})();
