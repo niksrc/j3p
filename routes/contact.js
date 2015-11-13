157226d6d45f4b6a5d6d2668b4773a758dfc0996
Nikhil Srivastava <niksrc@gmail.com>
2015-11-14 01:37:42 +0530

    Adds routes for about and contact page

---
+++ routes/contact.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var Contact = require('../models/contact');
+var router = express.Router();
+
+/*
+* These routes are for content in Contact page.
+*
+**/
+
+/* GET the content as we need only one we limit it to 1. */
+router.get('/items/', function(req, res, next) {
+  var limit = req.query.limit || 1;
+  var offset = req.query.offset || 0;
+
+  Contact.findAll({
+    limit: limit,
+    offset: offset,
+  })
+  .then(function(items){
+    res.send(items);
+  })
+});
+
+/* GET content with a given id. */
+router.get('/items/:id', function(req, res, next) {
+  var id = req.params.id;
+
+  if(id !== undefined)
+    Contact.findById(id)
+    .then(function(item){
+      res.send(item);
+    })
+  else
+    res.end();
+});
+
+router.post('/items/:id', function(req, res, next) {
+  var id = req.params.id;
+
+  Contact.findById(id)
+  .then(function(item){    
+      Contact.update(req.body,{
+        where:{
+          id:item.id
+        }
+      })
+      .then(function(affectedRows){
+        if(affectedRows == 1)
+          res.send({'error': false});
+        else
+          res.send({'error': true});
+      })
+  
+  })
+
+});
+
+module.exports = router;
